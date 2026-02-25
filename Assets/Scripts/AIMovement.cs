5cc039d825326b13a5234ce38725c291498b5893
vojtagabros <80451481+vojtagabros@users.noreply.github.com>
2026-02-25 15:14:43 +0100

    enemy navigation

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -7,22 +7,39 @@ public class AIMovement : MonoBehaviour
 {
     public GameObject destinationOfPlayer;
     public Transform[] spawnPoints;
+    public Transform[] destinations;
+    public float timeToWait = 5f;
 
     private NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        
-        //spawn
+
+        // spawn
         int randomIndex = Random.Range(0, spawnPoints.Length);
         Transform chosenSpawn = spawnPoints[randomIndex];
         agent.Warp(chosenSpawn.position);
+
+        StartCoroutine(UpdateDestinationRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(destinationOfPlayer.transform.position);
+        //agent.SetDestination(destinationOfPlayer.transform.position);
+        //agent.SetDestination(destinations[Random.Range(0, destinations.Length)].position);
+        //WaitUntil waitUntil = new WaitUntil(() => !agent.pathPending);
+        //yield return new WaitForSeconds(2);
     }
+    
+    IEnumerator UpdateDestinationRoutine()
+    {
+        while (true)
+        {
+            agent.SetDestination(destinations[Random.Range(0, destinations.Length)].transform.position);
+            yield return new WaitForSeconds(timeToWait);
+        }
+    }
+    
 }
